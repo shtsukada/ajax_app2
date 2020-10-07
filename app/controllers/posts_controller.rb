066ec65829ce878d32ef953fff3d881fd64be339
shtsukada <st01251988@gmail.com>
2020-10-07 15:52:49 +0900

    createメソッドの定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,27 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
   end
+
+  # def new
+  # end
+
+  # def create
+  #   Post.create(contents: params[:contents])
+  # end
 
   def new
     @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:contents)
   end
 
 end
